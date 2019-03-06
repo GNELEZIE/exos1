ef0ddec9434ce3e66e1441253098c5908203425d
GNELEZIE <47115156+GNELEZIE@users.noreply.github.com>
2019-03-06 19:34:12 +0000

    Add files via upload

---
+++ merci.php
@@ -0,0 +1,35 @@
+<h1 style="color:red;">MERCI D'AVOIR TESTER !!!</h1>
+
+
+
+
+<style>
+    
+    body{
+        background: #47287a;
+    }
+h1{
+    animation: Test 0.9s infinite;
+}
+@keyframes Test{
+    0%{opacity: 1;
+    color:red;
+    font-size: 30px;
+}
+    50%{opacity: 0;
+        color:orange;
+     background:green;
+
+    }
+    100%{opacity: 1;
+        color:white; 
+    }
+}
+    h1{
+        margin-top: 200px;
+        margin-left: 500px;
+        background:orange;
+        width: 450px;
+    }
+
+</style>
